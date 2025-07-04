0dc6ff7e1831945996f5744caef0013ea44e1e71
hungbui1204 <hungthe4869@gmail.com>
2025-07-04 16:12:04 +0700

    feat: setup supabase cli, add db migrations for get user's transactions

---
+++ supabase/migrations/20250704075337_create_get_user_transactions.sql
@@ -0,0 +1,30 @@
+-- migrate:up
+create or replace function public.get_user_transactions()
+returns setof json
+language sql
+security definer
+as $$
+  select json_build_object(
+    'id', t.id,
+    'amount', t.amount,
+    'note', t.note,
+    'created_at', t.created_at,
+    'wallet', json_build_object(
+      'id', w.id,
+      'name', w.name,
+      'icon_url', w.icon_url
+    ),
+    'category', json_build_object(
+      'id', c.id,
+      'name', c.name,
+      'icon_url', c.icon_url
+    )
+  )
+  from public.transactions t
+  join public.wallets w on t.wallet_id = w.id
+  join public.categories c on t.category_id = c.id
+  where t.user_id = auth.uid();
+$$;
+
+-- migrate:down
+drop function if exists public.get_user_transactions();
